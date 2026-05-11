53c99e27f88a666ee2f87d1a5c7cc9509990c1d1
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-05-11 11:24:24 -0400

    ideas

---
+++ src/sed.ts
@@ -0,0 +1,40 @@
+/*
+
+structural editor
+
+navigation
+
+* up nesting level
+* down nesting level
+
+* next item in nesting
+* previous item in nesting
+
+* atom snapping / auto-jump
+    * maintains preferred depth value
+    * when you jump left or right, if it's off the end, it jumps out until it's able to go to the right, and then jumps in until it hits an atom or reaches the same depth as before
+    * when you jump in or out, it sets the preferred depth value
+
+editing
+
+* wrap current item in list
+* wrap current item in object
+* add item before or after
+* delete current item
+* move current item back/forward
+* change atom type
+
+* edit string
+* edit number
+* toggle boolean
+
+* copy
+* cut
+* paste a copy
+* paste a reference
+
+* undo
+* redo
+
+
+*/
--- website/sed.ts
+++
@@ -1,6 +0,0 @@
-/*
-
-structural editor
-
-
-*/
