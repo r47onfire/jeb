bb45c077d90007b761ebea62e38c721c0249240d
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-06-22 16:16:31 -0400

    edit tests

--- test/linked_list.test.ts
+++ test/linked_list.test.ts
@@ -2,9 +2,11 @@ import { expect, test } from "bun:test";
 import { LinkedList, llPushArray, llToArray } from "../src";
 
 test("linked list from array to array roundtrips", () => {
+    expect(llToArray(llPushArray(null, [1, 2, 3]))).toEqual([1, 2, 3]);
+});
+
+test("linked list from array to array roundtrips", () => {
     var list: LinkedList<number> = llPushArray(null, [1, 2, 3]);
     expect(list).not.toBeNull();
     expect(list!.value).toEqual(1);
-    var arr = llToArray(list);
-    expect(arr).toEqual([1, 2, 3]);
 });
