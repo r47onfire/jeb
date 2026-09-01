bd7ed0565eb855107aa46da80f14ce9c3d25ccdb
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-09-01 16:02:46 -0400

    whoops

--- src/indextest.ts
+++ src/indextest.ts
@@ -2,7 +2,7 @@ import { define, makeJSFun } from "./define";
 import { Identifier } from "./utils";
 import { JebVM } from "./vm";
 
-export const makeTestRun = <T extends JebVM>(f: new () => T) => (testfun: (name: string, body: () => void) => void, name: string, testBody: (vm: JebVM, out: string[]) => void) => {
+export const makeTestRun = <T extends JebVM>(f: new () => T) => (testfun: (name: string, body: () => void) => void, name: string, testBody: (vm: T, out: string[]) => void) => {
     const vm = new f();
     const out: string[] = [];
     // simple print hook for the tests
